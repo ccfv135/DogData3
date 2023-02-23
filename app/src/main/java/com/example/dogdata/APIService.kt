ed2b4b08639cec9f7330684eb12644c9f4053b1a
Christian Fajardo <64758007+ccfv135@users.noreply.github.com>
2023-02-23 22:51:47 +0100

    Update APIService.kt

--- app/src/main/java/com/example/dogdata/APIService.kt
+++ app/src/main/java/com/example/dogdata/APIService.kt
@@ -7,4 +7,25 @@ import retrofit2.http.Url
 interface APIService {
     @GET
     suspend fun getDogsByBreeds(@Url url:String): Response<DogsResponse>
+     @GET("/example/example2/{id}/loquesea")
+    fun getCharacterByName2(@Path("id") id: String): Call<DogsResponse>
+
+
+    @GET("/example/example2/v2/loquesea")
+    fun getCharacterByName3(
+        @Query("pet") pet: String,
+        @Query("name") name: String
+    ): Call<DogsResponse>
+
+    @POST
+    fun getEVERYTHING(@Body exampleArisDto: ExampleArisDto): Call<*>
+
+
+    @Multipart
+    @POST
+    fun getEVERYTHING2(
+        @Part image: MultipartBody.Part,
+        @Part("example") myExample: String
+    ): Call<*>
 }
+data class ExampleArisDto(val name: String, val age: Int)
